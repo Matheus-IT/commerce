45cce5a8f36482caeb8b2c88065265c0c43c6cfa
Matheus <matheus1250.mc@gmail.com>
2020-09-17 19:50:09 -0300

    created model AuctionListing and the Comments, which is related

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,41 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Comments(models.Model):
+    content = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f'Content: {self.content}'
+
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=120)
+    currentPrice = models.DecimalField(max_digits=10, decimal_places=2)
+    imageUrl = models.CharField(max_length=1024, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    comments = models.ForeignKey(
+        Comments, 
+        on_delete=models.CASCADE, 
+        blank=True,
+        null=True,
+        related_name='auction'
+    )
+
+    CATEGORY_CHOICES = [
+        ('Fashion', 'fashion'),
+        ('Toys', 'toys'),
+        ('Electronics', 'electronics'),
+        ('Home', 'home')
+    ]
+    category = models.CharField(max_length=12, choices=CATEGORY_CHOICES, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Bid(models.Model):
+    pass
