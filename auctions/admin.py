ade1e6909617497c141af027752a374fc04b2996
Matheus <matheus1250.mc@gmail.com>
2020-09-18 14:24:02 -0300

    update list_display for AuctionListing

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import User, AuctionListing, Comments, Bids
 
+
+class AuctionListingAdmin(admin.ModelAdmin):
+	list_display = (
+		'id', 
+		'title',  
+		'currentPrice',  
+		'createdAt',  
+		'category'
+	)
+
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(AuctionListing)
+admin.site.register(AuctionListing, AuctionListingAdmin)
 admin.site.register(Comments)
 admin.site.register(Bids)
